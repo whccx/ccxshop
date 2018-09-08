c0f31538bf831ea1ae67f1bef4c72719eabe43e8
whccx <whccx@qq.com>
2018-09-08 13:29:29 +0800

    start

---
+++ apps/shop/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+from shop import views
+from shop import models
+app_name = 'shop'
+
+
+urlpatterns = [
+    #url(r'^1$', Show.as_view(),name='mydata_dp'),
+]
--- ccxshop/urls.py
+++ ccxshop/urls.py
@@ -1,21 +1,16 @@
-"""ccxshop URL Configuration
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+#网站ico
+from django.views.generic.base import RedirectView
+
+from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
+    #网站ico
+    url(r'^favicon.ico$',RedirectView.as_view(url=r'static/favicon.ico')),
+
     url(r'^admin/', admin.site.urls),
+    url(r'^shop/', include('shop.urls',namespace='shop')),
 ]
